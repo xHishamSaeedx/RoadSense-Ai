d1be052c053d3e7ffa48497015019c3e7c24e90d
Hisham <hishamsaeed2003@gmail.com>
2023-11-28 17:17:04 +0530

    configured basic urls and views and settings

--- Roadsense-Django/django_serv/django_serv/settings.py
+++ Roadsense-Django/django_serv/django_serv/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "videostream.apps.VideostreamConfig",
 ]
 
 MIDDLEWARE = [
--- Roadsense-Django/django_serv/videostream/views.py
+++ Roadsense-Django/django_serv/videostream/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("Hello, this is your app's home!")
 # Create your views here.
