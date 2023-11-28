8323f579d58c7eea1501fe6471cb8fd655b984a1
Hisham <hishamsaeed2003@gmail.com>
2023-11-29 00:15:43 +0530

    made few minor changes

--- Roadsense-Django/django_serv/videostream/views.py
+++ Roadsense-Django/django_serv/videostream/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.http import StreamingHttpResponse
+import cv2
+import threading
+import time
+import requests
+
 def home(request):
     return HttpResponse("Hello, this is your app's home!")
-# Create your views here.
+
