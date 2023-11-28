82317e86f8976781a3f019aacc8e09f10dc4d9dd
Hisham <95223059+xHishamSaeedx@users.noreply.github.com>
2023-11-28 17:24:02 +0530

    Delete Roadsense-Django directory

--- Roadsense-Django/django_serv/videostream/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    # Add your app's URLs here
-]
--- Roadsense-Django/django_serv/videostream/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Hello, this is your app's home!")
-# Create your views here.
