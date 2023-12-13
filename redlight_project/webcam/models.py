87e14385bc0599708316ddc1cb30c97f4316589a
Hisham <hishamsaeed2003@gmail.com>
2023-12-13 19:30:45 +0530

    added base mongodb

--- redlight_project/webcam/models.py
+++ redlight_project/webcam/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from db_connection import db
 
+person_collection = db['Person']
 # Create your models here.
--- redlight_project/webcam/views.py
+++ redlight_project/webcam/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import person_collection 
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("<h1>App is running</h1>")
+
+def add_person(request):
+
+    records = {
+        "first_name": "John",
+        "last_name": "smith",
+    }
+
+    person_collection.insert_one(records)
+    return HttpResponse("new person is added")
+
+
+def get_all_person(request):
+    persons = person_collection.find()
+    return HttpResponse(persons)
